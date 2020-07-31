7a9029513df68be3588246b37cd0fab711903c42
Enzo <e.radnai@hotmail.fr>
2020-07-31 10:07:45 +0200

    fix sql bugs

--- database/db.sql
+++ database/db.sql
@@ -1,2 +1,6 @@
-use todos;
-create table todos (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, title VARCHAR(100) NOT NULL, description VARCHAR(255), done TINYINT(1));
+USE todos;
+CREATE TABLE todos (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    title VARCHAR(100) NOT NULL, 
+    description VARCHAR(255), 
+    done TINYINT(1));
--- exo_python/src/app.py
+++ exo_python/src/app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 # mise en place des CORS
 CORS(app)
 # définition d'une variable de configuration pour sqlalchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@192.168.99.100:3306/todos'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@192.168.99.100:3306/todos'
 # initionalisation de la connexion à la base de données
 db = SQLAlchemy(app)
 # mise en place du controlleur pour les todos
