bba5b5cd31e19260b4a3a3702487d23d438b7992
Enzo <e.radnai@hotmail.fr>
2020-07-31 15:32:40 +0200

    finish proj

--- database/db.sql
+++ database/db.sql
@@ -3,4 +3,6 @@ CREATE TABLE todos (
     id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, 
     title VARCHAR(100) NOT NULL, 
     description VARCHAR(255), 
-    done TINYINT(1));
+    done TINYINT(1)
+    );
+
--- exo_python/src/services/todo_service.py
+++ exo_python/src/services/todo_service.py
@@ -1,12 +1,17 @@
 import repositories.todo_repo as todoRepo
 from models.todo import Todo
 import jsonpickle
+import json
 # défintion d'une méthode pour récupérer les todos dans la base de données avec le repo
 
 
 def get_todos():
-    todos = todoRepo.get_todos()
-    return jsonpickle.encode(todos)
+    result = todoRepo.get_todos()
+    todos = []
+    for todo in result:
+        print(todo)
+        # print(json.dumps(todo.__dict__))
+    return jsonpickle.encode(result)
 
 
 def get_todo_by_id(todo_id):
